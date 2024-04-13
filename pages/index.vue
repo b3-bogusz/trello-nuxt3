9a62119e759aa78f1b89f75963e0c2360b4fb1fa
p.bogusz <b3.bogusz@gmail.com>
2024-04-13 12:22:26 +0200

    add basic styles

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,23 @@ const store = useBoardStore();
 </script>
 
 <template>
-  <div>
-    <pre>{{ store.board }}</pre>
+  <div class="board-wrapper">
+    <main class="board">
+      <UContainer
+        v-for="column in store.board.columns"
+        :key="column.name"
+        class="column"
+      >
+        <h2 class="mb-4">{{ column.name }}</h2>
+        <ul>
+          <li v-for="task in column.tasks" :key="task.id">
+            <UCard class="mb-4">
+              <strong>{{ task.name }}</strong>
+              {{ task.description }}
+            </UCard>
+          </li>
+        </ul>
+      </UContainer>
+    </main>
   </div>
 </template>
