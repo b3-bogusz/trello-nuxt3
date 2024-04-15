f30aecf706c1ee807028b7e451ff80eb07790c71
p.bogusz <b3.bogusz@gmail.com>
2024-04-15 12:46:00 +0200

    add crud operations logic

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,51 @@
 const route = useRoute();
 
 const store = useBoardStore();
+
+const newColumnName = ref("");
+const editnameState = ref(false);
+
+function addColumn() {
+  store.addColumn(newColumnName.value);
+  newColumnName.value = "";
+}
+
+function editColumnName() {
+  editnameState.value = !editnameState.value;
+}
+
+function deleteColumn(columnIndex) {
+  store.deleteColumn(columnIndex);
+}
 </script>
 
 <template>
   <div class="board-wrapper">
     <main class="board">
       <UContainer
-        v-for="column in store.board.columns"
+        v-for="(column, columnIndex) in store.board.columns"
         :key="column.name"
         class="column"
       >
-        <h2 class="mb-4">{{ column.name }}</h2>
+        <div class="column-header mb-4">
+          <div>
+            <UInput v-if="editnameState" v-model="column.name" type="text" />
+            <h2 v-else>{{ column.name }}</h2>
+          </div>
+          <div>
+            <UButton
+              icon="i-heroicons-pencil-square-solid"
+              @click="editColumnName"
+              class="mr-2"
+            />
+            <UButton
+              icon="i-heroicons-trash-solid"
+              color="red"
+              @click="deleteColumn(columnIndex)"
+            />
+          </div>
+        </div>
+
         <ul>
           <li v-for="task in column.tasks" :key="task.id">
             <UCard class="mb-4">
@@ -21,6 +55,15 @@ const store = useBoardStore();
             </UCard>
           </li>
         </ul>
+      </UContainer>
+      <UContainer class="column">
+        <UInput
+          v-model="newColumnName"
+          type="text"
+          placeholder="Create new column"
+          icon="i-heroicons-plus-circle-solid"
+          @keyup.enter="addColumn"
+        ></UInput>
       </UContainer>
     </main>
   </div>
--- stores/boardStore.ts
+++ stores/boardStore.ts
@@ -1,10 +1,24 @@
 import { defineStore } from "pinia";
+import { useStorage } from "@vueuse/core";
 import boardData from "@/data/boardData.json";
 
 export const useBoardStore = defineStore("boardStore", () => {
-  const board = ref<{}>(boardData);
+  const board = useStorage("board", boardData);
+
+  const addColumn = (columnName) => {
+    board.value.columns.push({
+      name: columnName,
+      tasks: [],
+    });
+  };
+
+  const deleteColumn = (columnIndex) => {
+    board.value.columns.splice(columnIndex, 1);
+  };
 
   return {
     board,
+    addColumn,
+    deleteColumn,
   };
 });
